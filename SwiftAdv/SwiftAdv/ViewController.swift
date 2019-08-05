d7041986fb222980f0d6b149a7012878f4ae7370
Oxana Lobysheva <oxygens@Oxanas-MBP.home>
2019-08-05 05:20:21 -0400

    start commit

---
+++ SwiftAdv/SwiftAdv/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftAdv
+//
+//  Created by Oxana Lobysheva on 05/08/2019.
+//  Copyright © 2019 Oxana Lobysheva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
